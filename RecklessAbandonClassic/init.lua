ec365737e0d067241f304265ad6c8506f8d8b9f3
MotherGinger <29235654+MotherGinger@users.noreply.github.com>
2023-11-20 14:57:35 -0500

    Map zhTW to zhCN

--- RecklessAbandonClassic/init.lua
+++ RecklessAbandonClassic/init.lua
@@ -56,7 +56,7 @@ Engine[5] = E.DF.global
 _G.RecklessAbandonClassic = Engine
 
 do
-	local convert = {enGB = "enUS", esES = "esMX", itIT = "enUS"}
+	local convert = {enGB = "enUS", esES = "esMX", itIT = "enUS", zhTW = "zhCN"}
 	local gameLocale = convert[E.locale] or E.locale or "enUS"
 
 	function E:GetLocale()
